52c28daab80b1def3a6f469492a75525100f7b62
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-18 11:57:02 +0100

    A user can deposit an amount

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,11 @@ describe Account do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe '#deposit', :deposit do
+    it 'Increases the account balance by 10' do
+      subject.deposit(10)
+      expect(subject.balance).to eq(10)
+    end
+  end
 end

0650d666fa583346b59b5fef82a7116a7e81dc7e
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-18 14:55:58 +0100

    Add log method to Transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,6 +1,11 @@
- class Transaction
-   attr_reader :history
+class Transaction
+  attr_reader :history
+
   def initialize
     @history = []
   end
- end
+
+  def log(debit, credit, balance)
+    @history.push(date: Time.now.strftime("%d/%m/%Y"), debit: debit, credit: credit, balance: balance)
+  end
+end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,10 +1,18 @@
 require 'transaction'
 
 describe Transaction do
-
   describe '#history', :history do
     it 'Returns an empty array upon instantiation' do
       expect(subject.history).to eq([])
+    end
+  end
+
+  describe '#log', :log do
+    it 'Pushes a log to the history array' do
+      subject.log(0, 10, 10)
+      expect(subject.history).to eq([{
+        date: '18/06/2018', debit: 0, credit: 10, balance: 10
+                                    }])
     end
   end
 end

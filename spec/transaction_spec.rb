89d129898c7c469bec7b0a8b98b39452db599bc5
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-18 19:02:51 +0100

    Transaction responds to log with 4 arguments

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,6 +9,9 @@ describe Transaction do
   end
 
   describe '#log', :log do
+    it 'Takes 4 arguments' do
+      expect(subject).to respond_to(:log).with(4).arguments
+    end
 
     it 'Increases the history array length by 1' do
       expect { subject.log(date, 10, 0, 10) }.to change { subject.history.length }.by(1)

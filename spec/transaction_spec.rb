20feb39b63c79969fd829f2afd591e3fdced53d4
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-18 14:57:31 +0100

    Transaction logs credit and debit transactions

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,10 +8,17 @@ describe Transaction do
   end
 
   describe '#log', :log do
-    it 'Pushes a log to the history array' do
+    it 'Pushes a credit log to the history array' do
       subject.log(0, 10, 10)
       expect(subject.history).to eq([{
         date: '18/06/2018', debit: 0, credit: 10, balance: 10
+                                    }])
+    end
+
+    it 'Pushes a debit log to the history array' do
+      subject.log(10, 0, 10)
+      expect(subject.history).to eq([{
+        date: '18/06/2018', debit: 10, credit: 0, balance: 10
                                     }])
     end
   end

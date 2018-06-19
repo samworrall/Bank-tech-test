962f33906e69eb600757ddb1116d0369b7de83f0
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-19 09:41:13 +0100

    Printer pretty prints a statement

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,4 +1,9 @@
 class Printer
   def pretty_print(statement)
+    printed_statement = "Date || Credit || Debit || Balance\n"
+    statement.reverse.each do |transaction|
+      printed_statement += "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}\n"
+    end
+    p printed_statement
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,9 +1,16 @@
 require 'printer'
 
 describe Printer do
+  let(:statement) { [{ date: '18/06/2018', credit: 20, debit: 0, balance: 20 },
+     { date: '18/06/2018', credit: 0, debit: 10, balance: 10 }] }
+
   describe '#pretty_print', :pretty_print do
     it 'Takes 1 argument' do
       expect(subject).to respond_to(:pretty_print).with(1).arguments
+    end
+
+    it 'Pretty prints a statement' do
+      expect(subject.pretty_print(statement)).to eq("Date || Credit || Debit || Balance\n18/06/2018 || 0 || 10 || 10\n18/06/2018 || 20 || 0 || 20\n")
     end
   end
 end

843a02d6cc5340c59f20eb7d429cb4e950586824
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-20 17:38:57 +0100

    Feature test printing statement

---
+++ spec/feature_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/account.rb'
+require_relative '../lib/transaction_log.rb'
+require_relative '../lib/printer.rb'
+
+describe 'Bank Account', :feature do
+
+  it 'Prints the desired statement' do
+    account = Account.new
+    date = Time.now.strftime('%d/%m/%Y')
+    account.deposit(50)
+    account.deposit(50)
+    account.withdraw(50)
+    expect { account.view_statement }.to output(
+      <<~HEREDOC
+      Date || Credit || Debit || Balance
+      #{date} || 0 || 50 || 50
+      #{date} || 50 || 0 || 100
+      #{date} || 50 || 0 || 50\n
+      HEREDOC
+    ).to_stdout
+  end
+end
